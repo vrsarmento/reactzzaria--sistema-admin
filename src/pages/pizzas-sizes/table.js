6fae2ccde71b193f8b0c43485d04dd26fd254041
Victor Rocha <vrsarmento@gmail.com>
2021-04-06 01:19:20 -0300

    Adding action buttons to PizzasSizes page's layout

--- src/pages/pizzas-sizes/table.js
+++ src/pages/pizzas-sizes/table.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 import {
+  Button as MaterialButton,
+  Grid,
   Table,
   TableBody,
   TableCell,
   TableRow
 } from '@material-ui/core'
+import {
+  Add,
+  Delete,
+  Edit
+} from '@material-ui/icons'
 import {
   TableContainer,
   TableTitle,
@@ -19,9 +27,19 @@ function TablePizzasSizes () {
 
   return (
     <TableContainer>
-      <TableTitle>
-        Tamanhos cadastrados
-      </TableTitle>
+      <TitleContainer>
+        <Grid item>
+          <TableTitle>
+            Tamanhos cadastrados
+          </TableTitle>
+        </Grid>
+
+        <Grid item>
+          <Button startIcon={<Add />} color='primary'>
+            Adicionar novo tamanho
+          </Button>
+        </Grid>
+      </TitleContainer>
 
       <Table>
         <THead>
@@ -30,18 +48,29 @@ function TablePizzasSizes () {
             <Th>Tamanho</Th>
             <Th>Fatias</Th>
             <Th>Sabores</Th>
+            <Th />
           </TableRow>
         </THead>
 
         <TableBody>
           {pizzasSizes?.map(pizza => (
             <TableRow key={pizza.id}>
-              <TableCell>{pizza.name}</TableCell>
+              <TableCell><b>{pizza.name}</b></TableCell>
               <TableCell>{pizza.size} cm</TableCell>
               <TableCell>{pizza.slices} fatias</TableCell>
               <TableCell>
                 {pizza.flavours}
                 {singularOrPlural(pizza.flavours, ' sabor', ' sabores')}
+              </TableCell>
+
+              <TableCell align='right'>
+                <Button startIcon={<Edit />}>
+                  Editar
+                </Button>
+
+                <Button startIcon={<Delete />} color='secondary'>
+                  Remover
+                </Button>
               </TableCell>
             </TableRow>
           ))}
@@ -50,5 +79,23 @@ function TablePizzasSizes () {
     </TableContainer>
   )
 }
+
+const TitleContainer = styled(Grid).attrs({
+  container: true,
+  justify: 'space-between',
+  alignItems: 'center'
+})`
+  padding: ${({ theme }) => theme.spacing(3)}px;
+
+  ${TableTitle} {
+    padding: 0;
+  }
+`
+
+const Button = styled(MaterialButton).attrs({
+  variant: 'contained'
+})`
+  margin-left: ${({ theme }) => theme.spacing(2)}px;
+`
 
 export default TablePizzasSizes

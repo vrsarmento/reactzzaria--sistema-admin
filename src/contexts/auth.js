876a8c438ee0d54b1695592545abfc5ac793589e
Victor Rocha <vrsarmento@gmail.com>
2021-04-09 19:25:55 -0300

    Added the insertion of the user info in the database to define his/her role

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
-import React, { createContext, useCallback, useState } from 'react'
+import React, { createContext, useCallback, useEffect, useState } from 'react'
 import t from 'prop-types'
-import firebase from 'services/firebase'
+import firebase, { db } from 'services/firebase'
 
 const AuthContext = createContext()
 
@@ -9,6 +9,20 @@ function AuthProvider ({ children }) {
     isUserLoggedIn: false,
     user: null
   })
+
+  useEffect(() => {
+    const uid = userInfo.user?.uid || 'EMPTY'
+    db.collection('users').doc(uid).get().then(doc => {
+      if (doc.exists || uid === 'EMPTY') {
+        return
+      }
+      db.collection('users').doc(uid).set({
+        email: userInfo.user.email,
+        name: userInfo.user.displayName,
+        role: 'user'
+      })
+    })
+  }, [userInfo])
 
   const login = useCallback(() => {
     const provider = new firebase.auth.GithubAuthProvider()
